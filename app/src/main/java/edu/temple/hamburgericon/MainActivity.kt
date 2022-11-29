1c6cd638d506b47b8b0728293f5ec75c27ace9d4
levinoscar <oscarlevin@live.com>
2022-11-29 15:19:40 -0500

    End of lecture

--- app/src/main/java/edu/temple/hamburgericon/MainActivity.kt
+++ app/src/main/java/edu/temple/hamburgericon/MainActivity.kt
@@ -1,11 +1,70 @@
 package edu.temple.hamburgericon
 
+import android.content.Intent
+
+import android.net.Uri
+
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 
+import android.view.Menu
+
+import android.view.MenuItem
+
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+
+            R.id.action_take_picture -> Toast.makeText(this, "Taking picture", Toast.LENGTH_SHORT).show()
+
+            R.id.action_help -> startActivity(Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://www.temple.edu")))
+
+            R.id.action_delete_picture -> {
+
+                AlertDialog.Builder(this)
+
+                    .setTitle("Confirmation")
+
+                    .setMessage("Are you sure you want to delete? This action cannot be undone.")
+
+                    .setPositiveButton("Yes") {_,_ -> Toast.makeText(this, "Deleting picture", Toast.LENGTH_SHORT).show()}
+
+                    .setNegativeButton("Never mind") {dialog,_ -> dialog.cancel()}
+
+                    .create()
+
+                    .show()
+
+            }
+
+            else -> return false
+
+        }
+
+        return true
+
+    }
+
 }
